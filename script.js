469e35de63fb2106bc09ce96511abeed6f5dc420
Adam Reshamwala <reshamwala.adam@gmail.com>
2025-06-11 23:47:45 +0530

    Update

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function sendWhatsAppMessage() {
-  const name = document.getElementById("name").value;
-  const service = document.getElementById("service").value;
-  const message = document.getElementById("message").value;
-
-  const phone = "918839824832"; // Replace with your WhatsApp number
-  const text = `Hello, I am ${name}.\nI need help with: ${service}.\n\nMy message:\n${message}`;
-  const encoded = encodeURIComponent(text);
-
-  const url = `https://wa.me/${phone}?text=${encoded}`;
-  window.open(url, "_blank");
-
-  return false;
-}
 function toggleMenu() {
   document.getElementById('navLinks').classList.toggle('active');
 }
